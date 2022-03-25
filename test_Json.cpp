3f3812b275ce45eb558f57d2c27858cb271de62a
misanthropique <80142735+misanthropique@users.noreply.github.com>
2022-03-25 12:08:49 -0500

    Update and rename test_json.cpp to test_Json.cpp

--- test_Json.cpp
+++ test_Json.cpp
@@ -83,3 +83,9 @@ TEST( JsonValueConstructor, ObjectTypeMoveConstructorShouldSetJsonValueToTypeObj
 
 	
 }
+
+int main( int argc, char** argv )
+{
+	::testing::InitGoogleTest( &argc, argv );
+	return RUN_ALL_TESTS();
+}
